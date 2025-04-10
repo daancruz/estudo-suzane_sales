19681ece677235a41027db3c048fd956a64f48ca
Weener Brasil Estoque (1661) Unidade GRSA <unop1661e@grsa.com.br>
2025-04-10 10:16:48 -0300

    Ajustes no caminho para acessar as páginas no GH Pages

--- js/noticias.js
+++ js/noticias.js
@@ -11,10 +11,10 @@ document.addEventListener("DOMContentLoaded", () => {
             const divCol = document.createElement("div");
 
             // NOTICIAS DA HOME
-            if (path === "/" || path.endsWith("/index.html") || path === "/faq.html") {
+            if (path === "/" || path.endsWith("/index.html") || path.endsWith === "/faq.html") {
                 divCol.className = "swiper-slide";
                 divCol.innerHTML = `
-                    <a href="/blog/detalhes/${dado.id}" class="swiper-slide" id="${dado.id}">
+                    <a href="/estudo-suzane_sales/blog/detalhes/${dado.id}" class="swiper-slide" id="${dado.id}">
                         <div class="custom__card" style="background-image: url(img/noticia${dado.id}.png);">
                             <div class="content card-overlay position-absolute bottom-0 px-3">
                                 <h6 class="title">${dado.title}</h6>
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
                             <h6 class="title">${dado.title}</h6>
                             <p class="text">${dado.subtitle}</p>
                         </div>
-                        <a class="link position-absolute bottom-0 my-3" href="/blog/detalhes.html?id=${dado.id}">Ler mais</a>
+                        <a class="link position-absolute bottom-0 my-3" href="/estudo-suzane_sales/blog/detalhes.html?id=${dado.id}">Ler mais</a>
                     </div>
                 `;
                 section.appendChild(divCol);
